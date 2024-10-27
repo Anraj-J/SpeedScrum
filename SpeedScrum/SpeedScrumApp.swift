c356345f24be5e6850f191db8fda95b30b363f34
Anraj Jeji <ajjeji@gmail.com>
2024-10-27 15:49:49 -0400

    Initial Commit

---
+++ SpeedScrum/SpeedScrumApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedScrumApp.swift
+//  SpeedScrum
+//
+//  Created by Anraj Jeji on 2024-10-27.
+//
+
+import SwiftUI
+
+@main
+struct SpeedScrumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
